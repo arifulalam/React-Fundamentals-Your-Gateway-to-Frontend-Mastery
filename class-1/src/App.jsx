ec6eaf0c3c0cf93d36fe474dcd89e0df6dd69502
Ariful Alam <arifulalam@users.noreply.github.com>
2024-06-03 20:05:40 +0600

    class 01 initial

---
+++ class-1/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import { Box } from './components/Box'
+
+function App() {
+  return (
+    <>
+      <h1>Hello World!</h1>
+      <Box></Box>
+    </>
+  )
+}
+
+export default App
